8fdae9f91ca9044888d441a3be3c151c74e91774
鲷鱼 <2382391761@qq.com>
2019-06-12 21:04:15 +0800

    Create main.cpp

---
+++ project/main.cpp
@@ -0,0 +1,19 @@
+//
+//  main.cpp
+//  project
+//
+//  Created by s20181102933 on 2019/6/12.
+//  Copyright © 2019 s20181102933. All rights reserved.
+//
+
+#include <iostream>
+#include <cmath>
+using namespace std;
+int main(){
+    int i;
+    cin>>i;
+    if(i=612){
+        cout<<"Mac10"<<endl;
+    }
+    return 0;
+}
