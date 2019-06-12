c77232a886ead281e4729338c6c24d62da37d389
鲷鱼 <2382391761@qq.com>
2019-06-12 21:31:06 +0800

    冒泡排序
    1

--- project/main.cpp
+++ project/main.cpp
@@ -11,12 +11,24 @@
 using namespace std;
 int main(){
     int i;
-    cin>>i;
-    if(i=1){
-        cout<<"Qie"<<endl;
+    int a[10];
+    for(i=0;i<10;i++){
+        cin>>a[i];
     }
-    else{
-        cout<<"Mac10"<<endl;
+    for(i=0;i<10;i++){
+        int s;
+        int j;
+        for(j=1;j<10;j++){
+        if(a[j]<a[j-1]){
+            s=a[j];
+            a[j]=a[j-1];
+            a[j-1]=s;
+        }
+        }
+    }
+    for(i=0;i<10;i++){
+        cout<<a[i]<<endl;
     }
     return 0;
+
 }
