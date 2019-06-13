298469b05cc2392ff24d0747d5bcd9ad78ab8a43
鲷鱼 <2382391761@qq.com>
2019-06-13 11:49:43 +0800

    Create main.cpp

---
+++ Project/Project/鲷鱼/鲷鱼/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  鲷鱼
+//
+//  Created by s20181102933 on 2019/6/13.
+//  Copyright © 2019 s20181102933. All rights reserved.
+//
+
+#include <iostream>
+using namespace std;
+int main(){
+    int i=1;
+    cout<<i<<endl;
+    return 0;
+}
