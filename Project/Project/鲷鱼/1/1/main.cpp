11cc00327365ed843c89f2ae3d28a391ba60e016
鲷鱼 <2382391761@qq.com>
2019-06-19 20:47:00 +0800

    Update main.cpp

--- Project/Project/鲷鱼/1/1/main.cpp
+++ Project/Project/鲷鱼/1/1/main.cpp
@@ -12,12 +12,12 @@ using namespace std;
 
 int main()
 {
-    ofstream ef("/Users/project/Project/Project/ef.txt");
-    if (ef.is_open())
+    ofstream ef("/Users/s20181102933/Desktop/project/Project/Project/鲷鱼/1/1/data.txt");
+    ifstream ief("/Users/s20181102933/Desktop/project/Project/Project/鲷鱼/1/1/data.txt");
+    //cout<<"c ="<<a+b<<endl;
+    if(ef.is_open())
     {
-            ef <<"This is a line.\n";
-            ef <<"This is another line.\n";
-            ef.close();
+        ef<<"xing"<<endl;
     }
     return 0;
 }
